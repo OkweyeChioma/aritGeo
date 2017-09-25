62c91a20f62d744e7ea4484e077d06a53a095c56
chioma okweye <okweyechioma@yahoo.com>
2017-09-25 18:23:08 +0100

    working unittest

--- src/main.js
+++ src/main.js
@@ -1,35 +1,31 @@
 'use strict';
 module.exports ={
-  function aritGeo(arr){
-    let hold_CD =[]
-  let hold_CM =[]
-  if(arr.length ===0){
-    return 0
-  }
-  let bb = arr.every(x=> x.constructor ===Number)
-    if(!bb){
+  aritGeo(arr){
+    if(arr.length ===0){
+      return 0;
+    }
+    if(arr.every(num=> num.constructor !==Number)){
       return -1;
     }
-  for(let i=0;i<arr.length -1; i++){
-    let commonDiff = arr[i+1] - arr[i]
-    hold_CD.push(commonDiff)
-    
-    commonMult = arr[i+1] / arr[i]
-    hold_CM.push(commonMult)
-    
-  }
-  let uniqueCD = new Set(hold_CD)
-  if(uniqueCD.size ===1){
-    return 'Arithmetic'
-  }
-  let uniqueCM = new Set(hold_CM)
-  if(uniqueCM.size ===1){
-    return 'Geometric'
-  }else{
-    return -1
-  }
-  
-  
-}
+    let hold_CD=[]
+    let hold_CM=[]
+    for(let i=0; i<arr.length-1; i++){
+      let commonDiff =arr[i+1] - arr[i]
+      hold_CD.push(commonDiff);
+      let commonDiv = arr[i+1] / arr[i]
+      hold_CM.push(commonDiv);
+    }
+    let uniqueCD = new Set(hold_CD)
+    if(uniqueCD.size ===1){
+      return 'Arithmetic';
+    }
+    let uniqueCM = new Set(hold_CM);
+    if(uniqueCM.size ===1){
+      return 'Geometric'
+    }else{
+      return -1;
+    }
   }
 }
+  
+    
